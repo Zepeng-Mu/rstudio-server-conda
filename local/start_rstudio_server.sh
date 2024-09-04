8af8bdbf3328d4dab57dac3795a916eadc6e10a1
Zepeng Mu <zmu@broadinstitute.org>
2024-09-03 20:10:27 -0400

    changes

--- local/start_rstudio_server.sh
+++ local/start_rstudio_server.sh
@@ -13,14 +13,9 @@ rm -f $COOKIE_KEY_PATH
 mkdir -p $(dirname $COOKIE_KEY_PATH)
 
 # Rserver >= version 1.3 requires the --auth-revocation-list-dir parameter
-if [ $(sed -n '/^1.3./p;q' /usr/lib/rstudio-server/VERSION) ] ;
-then
-  REVOCATION_LIST_DIR=/tmp/rstudio-server/${USER}_revocation-list-dir
-  mkdir -p $REVOCATION_LIST_DIR
-  REVOCATION_LIST_PAR="--auth-revocation-list-dir=$REVOCATION_LIST_DIR"
-else
-  REVOCATION_LIST_PAR=""
-fi
+REVOCATION_LIST_DIR=/tmp/rstudio-server/${USER}_revocation-list-dir
+mkdir -p $REVOCATION_LIST_DIR
+REVOCATION_LIST_PAR="--auth-revocation-list-dir=$REVOCATION_LIST_DIR"
 
 python -c 'import uuid; print(uuid.uuid4())' > $COOKIE_KEY_PATH
 chmod 600 $COOKIE_KEY_PATH
@@ -37,7 +32,7 @@ export RETICULATE_PYTHON=$CONDA_PREFIX/bin/python
 # store the current path in database config
 sed -i "s,directory=.*,directory=\/tmp\/rstudio-server\/${USER}_database," $CWD/database.conf
 
-/usr/lib/rstudio-server/bin/rserver --server-daemonize=0 \
+/apps/lib-osver/Rstudio-server/2023.12.1-402/bin/rserver --server-daemonize=0 \
   --www-port=$1 \
   --secure-cookie-key-file=$COOKIE_KEY_PATH \
   --server-pid-file="$CWD/rstudio-server.pid" \
